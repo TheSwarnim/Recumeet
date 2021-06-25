aae955a08b2f962e2407ca0497bd447ad4457e5b
ayushvatsal <vatsalayush31@gmail.com>
2021-06-25 15:19:17 +0530

    splash screen added

--- app/src/main/java/com/hackathon/recumeet/SplashScreenActivity.java
+++ app/src/main/java/com/hackathon/recumeet/SplashScreenActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,11 +20,24 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        if(fUser == null){
-            startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
-        } else {
-            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-        }
-        finish();
+        ImageView bg = findViewById(R.id.img);
+        ImageView logo = findViewById(R.id.logo);
+        ImageView name = findViewById(R.id.text_logo);
+        logo.animate().translationY(2800f).setDuration(1300).setStartDelay(1000);
+        bg.animate().translationY(-2800f).setDuration(1300).setStartDelay(1000);
+        name.animate().translationY(2800f).setDuration(1300).setStartDelay(1000);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(fUser == null){
+                    startActivity(new Intent(SplashScreenActivity.this, LoginActivity.class));
+                } else {
+                    startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                }
+                finish();
+            }
+        },2300);
     }
 }
