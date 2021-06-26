799ad274aac0d8c050b5e18e54591e2df779c7b9
shreyas-085 <shreyask085@gamil.com>
2021-06-26 11:24:56 +0530

    Post Bug Solved

--- app/src/main/java/com/hackathon/recumeet/Fragments/FeedFragment.java
+++ app/src/main/java/com/hackathon/recumeet/Fragments/FeedFragment.java
@@ -3,13 +3,11 @@ package com.hackathon.recumeet.Fragments;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -47,7 +45,7 @@ public class FeedFragment extends Fragment {
     private RecyclerView recyclerViewPosts;
     private PostAdapter postAdapter;
     private List<Post> postList;
-    private EditText search;
+    private LinearLayout search;
 
     private TextView isFollow;
 
