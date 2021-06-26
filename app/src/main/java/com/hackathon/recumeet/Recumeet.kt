940713b419416b022f69c6e8a588ef43078f9f58
Swarnim Pratap Singh <swarnim132@gmail.com>
2021-06-26 15:25:36 +0530

    Added notification functionlity in chat

--- app/src/main/java/com/hackathon/recumeet/Recumeet.kt
+++ app/src/main/java/com/hackathon/recumeet/Recumeet.kt
@@ -6,14 +6,25 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.client.notifications.handler.ChatNotificationHandler
+import io.getstream.chat.android.client.notifications.handler.NotificationConfig
 import io.getstream.chat.android.livedata.ChatDomain
 
 class Recumeet : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        val notificationsConfig = NotificationConfig(
+            firebaseMessageIdKey = "message_id",
+            firebaseChannelIdKey = "channel_id",
+            firebaseChannelTypeKey = "channel_type",
+        )
+
         val client =
-            ChatClient.Builder(getString(R.string.api_key), this).logLevel(ChatLogLevel.ALL).build()
+            ChatClient.Builder(getString(R.string.api_key), this).notifications(
+                ChatNotificationHandler(this, notificationsConfig)
+            ).logLevel(ChatLogLevel.ALL).build()
+
         ChatDomain.Builder(client, this).build()
 
         Firebase.database.setPersistenceEnabled(true)
