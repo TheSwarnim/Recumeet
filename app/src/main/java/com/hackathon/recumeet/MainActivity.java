d35348c0194cfd22daa621293701f96755774895
shreyas-085 <shreyask085@gamil.com>
2021-06-27 00:11:37 +0530

    add resume feture added

--- app/src/main/java/com/hackathon/recumeet/MainActivity.java
+++ app/src/main/java/com/hackathon/recumeet/MainActivity.java
@@ -1,22 +1,21 @@
 package com.hackathon.recumeet;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.hackathon.recumeet.fragments.FeedFragment;
 import com.hackathon.recumeet.fragments.ChannelFragment;
+import com.hackathon.recumeet.fragments.FeedFragment;
 import com.hackathon.recumeet.fragments.MeetFragment;
 import com.hackathon.recumeet.fragments.ProfileFragment;
 import com.hackathon.recumeet.fragments.SearchFragment;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     private Toast backToast;
     BottomNavigationView bottomNavigationView;
-    Fragment sFragment = null;
-    FirebaseUser firebaseUser;
+    Deque<Integer> integerDeque = new ArrayDeque<>(5);
+    boolean flag = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,38 +32,56 @@ public class MainActivity extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
 
-        Init();
+        bottomNavigationView = findViewById(R.id.bottom_nav);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragement_container, new FeedFragment()).commit();
+        integerDeque.push(R.id.nav_feed);
+        loadFragment(new FeedFragment());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (integerDeque.contains(id)) {
+                if (id == R.id.nav_feed) {
+                    if (integerDeque.size() != 1) {
+                        if (flag) {
+                            integerDeque.addFirst(R.id.nav_feed);
+                            flag = false;
+                        }
+                    }
+                }
+                integerDeque.remove(id);
+            }
+            integerDeque.push(id);
+            loadFragment(getFragment(item.getItemId()));
+            return true;
+        });
     }
 
+    private Fragment getFragment(int itemId) {
+        if (R.id.nav_feed == itemId) {
+            bottomNavigationView.getMenu().getItem(0).setChecked(true);
+            return new FeedFragment();
+        } else if (R.id.nav_search == itemId) {
+            bottomNavigationView.getMenu().getItem(1).setChecked(true);
+            return new SearchFragment();
+        } else if (R.id.nav_meet == itemId) {
+            bottomNavigationView.getMenu().getItem(2).setChecked(true);
+            return new MeetFragment();
+        } else if (R.id.nav_chat == itemId) {
+            bottomNavigationView.getMenu().getItem(3).setChecked(true);
+            return new ChannelFragment();
+        } else if (R.id.nav_settings == itemId) {
+            bottomNavigationView.getMenu().getItem(4).setChecked(true);
+            return new ProfileFragment();
+        }
 
-    private final BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
-            item -> {
-                if (item.getItemId() == R.id.nav_feed) {
-                    sFragment = new FeedFragment();
-                } else if (item.getItemId() == R.id.nav_chat) {
-                    sFragment = new ChannelFragment();
-                } else if (item.getItemId() == R.id.nav_meet) {
-                    sFragment = new MeetFragment();
-                } else if (item.getItemId() == R.id.nav_search) {
-                    sFragment = new SearchFragment();
-                } else if (item.getItemId() == R.id.nav_settings) {
-                    sFragment = new ProfileFragment();
-                }
+        bottomNavigationView.getMenu().getItem(0).setChecked(true);
+        return new FeedFragment();
+    }
 
-                if (sFragment != null) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragement_container, sFragment).commit();
-                }
-
-                return true;
-            };
-
-    private void Init() {
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
-        bottomNavigationView = findViewById(R.id.bottom_navigation);
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment, fragment, fragment.getClass().getSimpleName())
+                .commit();
     }
 
     @Override
--- app/src/main/java/com/hackathon/recumeet/fragments/ProfileFragment.java
+++ app/src/main/java/com/hackathon/recumeet/fragments/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.hackathon.recumeet.fragments;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,15 +10,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,31 +26,38 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.hackathon.recumeet.Adapter.PhotoAdapter;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.hackathon.recumeet.Feed.AddPostActivity;
 import com.hackathon.recumeet.Models.Post;
 import com.hackathon.recumeet.Models.User;
 import com.hackathon.recumeet.R;
 import com.hackathon.recumeet.UserAuth.LoginActivity;
 import com.hackathon.recumeet.UserProfile.ConnectionActivity;
 import com.hackathon.recumeet.UserProfile.EditProfile2Activity;
+import com.hackathon.recumeet.UserProfile.ProfilePosts;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 import io.getstream.chat.android.client.ChatClient;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
 
     View view;
     private ImageView profile_pic, more;
     private TextView fullName, bio, noOfPosts, noOffollowers, noOffollowing, username;
-    private Button editProfile;
+    private Button editProfile, viewPosts;
 
-    private LinearLayout followers, following;
+    private LinearLayout post, followers, following, resume;
 
     private FirebaseUser firebaseUser;
     private DatabaseReference ref1;
@@ -57,6 +65,9 @@ public class ProfileFragment extends Fragment {
     private AlertDialog.Builder ad;
 
     private ChatClient chatClient = ChatClient.instance();
+
+    StorageReference storageReference;
+    DatabaseReference databaseReference;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -66,6 +77,9 @@ public class ProfileFragment extends Fragment {
 
         ref1 = FirebaseDatabase.getInstance().getReference().child("Follow").child(firebaseUser.getUid());
 
+        viewPosts.setOnClickListener(v -> openPosts());
+        post.setOnClickListener(v -> openPosts());
+
         more.setOnClickListener(v -> {
             ad = new AlertDialog.Builder(getContext());
             ad.setTitle("Logout");
@@ -74,7 +88,7 @@ public class ProfileFragment extends Fragment {
 
             ad.setPositiveButton(android.R.string.yes, (dialog, which) -> {
                 // chat logout
-                if(chatClient.getCurrentUser() != null){
+                if (chatClient.getCurrentUser() != null) {
                     chatClient.disconnect();
                 }
                 //firebase logout
@@ -105,6 +119,8 @@ public class ProfileFragment extends Fragment {
             intent.putExtra("Uid", firebaseUser.getUid());
             startActivity(intent);
         });
+        
+        resume.setOnClickListener(v -> UploadDoc());
 
         getUserData();
         setNoOfFollowers(noOffollowers);
@@ -114,6 +130,54 @@ public class ProfileFragment extends Fragment {
         return view;
     }
 
+    private void UploadDoc() {
+        selectPDF();
+    }
+
+    private void selectPDF() {
+        Intent intent = new Intent();
+        intent.setType("application/pdf");
+        intent.setAction(intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "PDF FILE SELECT"), 12);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 12 && resultCode==RESULT_OK && data != null && data.getData() != null){
+            uploadPDF(data.getData());
+        }
+    }
+
+    private void uploadPDF(Uri data) {
+        StorageReference reference = storageReference.child("resume" + System.currentTimeMillis() + ".pdf");
+
+        reference.putFile(data).addOnSuccessListener(taskSnapshot -> {
+            Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+            while (!uriTask.isComplete());
+            Uri uri = uriTask.getResult();
+
+            final DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users");
+            String uid = FirebaseAuth.getInstance().getUid();
+
+            assert uid != null;
+            assert uri != null;
+            ref.child(uid).child("resumeLink").setValue(uri.toString()).addOnCompleteListener(task -> {
+                Toast.makeText(getContext(), "Resume Added Successfully", Toast.LENGTH_SHORT).show();
+            });
+
+        }).addOnProgressListener(snapshot -> {
+            double progress = (100.0 * snapshot.getBytesTransferred()) / snapshot.getTotalByteCount();
+        });
+    }
+
+    private void openPosts() {
+        Intent intent = new Intent(getContext(), ProfilePosts.class);
+        intent.putExtra("publisherId", firebaseUser.getUid());
+        startActivity(intent);
+    }
+
+
     private void getPostData(final TextView noOfPosts) {
         DatabaseReference ref5 = FirebaseDatabase.getInstance().getReference().child("posts");
         ref5.keepSynced(true);
@@ -121,7 +185,15 @@ public class ProfileFragment extends Fragment {
         ref5.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                noOfPosts.setText(dataSnapshot.getChildrenCount() + "");
+                int i = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Post post = snapshot.getValue(Post.class);
+                    assert post != null;
+                    if (post.getPublisher().equals(firebaseUser.getUid()) && post.getImageUri() != null) {
+                        i++;
+                    }
+                }
+                noOfPosts.setText(String.valueOf(i));
             }
 
             @Override
@@ -205,18 +277,23 @@ public class ProfileFragment extends Fragment {
         fullName = view.findViewById(R.id.full_name);
         bio = view.findViewById(R.id.bio);
         editProfile = view.findViewById(R.id.editprofile);
+        viewPosts = view.findViewById(R.id.view_post);
 
         noOfPosts = view.findViewById(R.id.no_posts);
         noOffollowers = view.findViewById(R.id.no_followers);
         noOffollowing = view.findViewById(R.id.no_following);
 
+        post = view.findViewById(R.id.post);
         followers = view.findViewById(R.id.followers);
         following = view.findViewById(R.id.following);
+        resume = view.findViewById(R.id.resume);
 
         username = view.findViewById(R.id.user_name);
         more = view.findViewById(R.id.profile_menu);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference("resume");
 
     }
 }
